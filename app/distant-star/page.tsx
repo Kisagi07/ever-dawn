31edd6335d14812e9905d1295dde5f8c915e8dc7
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-04-14 21:58:11 +0700

    feat: add the field for adding new star

--- app/distant-star/page.tsx
+++ app/distant-star/page.tsx
@@ -1,4 +1,7 @@
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import BreadCrumb from "../components/BreadCrumb";
+import Button from "../components/Button";
+import FloatingText from "../components/FloatingText";
 
 export default function Page() {
   return (
@@ -6,6 +9,14 @@ export default function Page() {
       <section className="max-w-7xl bg-white shadow-lg rounded-lg p-8 mx-auto space-y-8">
         <BreadCrumb />
         <p>Each hour lights the path ahead.</p>
+        <div className="flex gap-4 items-center">
+          <Button className="mt-3">
+            <PlusCircleIcon className="size-6" />
+            New Star
+          </Button>
+          <FloatingText label="Star Name" />
+          <FloatingText defaultValue="0" label="Distance" endOfValue="Hours" />
+        </div>
       </section>
     </div>
   );
