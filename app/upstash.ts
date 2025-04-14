4fe45d78c533023a43afca444ea1ace1827dd5eb
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-04-15 00:11:48 +0700

    feat: update upstash token

--- app/upstash.ts
+++ app/upstash.ts
@@ -2,7 +2,7 @@ import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
   url: "https://allowed-tuna-20630.upstash.io",
-  token: "VCWAAIjcDE2YWFkZjg5MzcyNjQ0YjhlYjgxOWY2MzczNjA2NTRmNHAxMA",
+  token: "AVCWAAIjcDE2YWFkZjg5MzcyNjQ0YjhlYjgxOWY2MzczNjA2NTRmNHAxMA",
 });
 
 export default redis;
