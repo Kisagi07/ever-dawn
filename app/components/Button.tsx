1e8ce81ae7ea49ce76b12d59d7a2b90bd35c71ee
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-04-14 21:57:49 +0700

    feat: add className optional parameter

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -4,14 +4,16 @@ import { ReactNode } from "react";
 const Button = ({
   type = "button",
   children,
+  className = "",
 }: {
   type?: "button" | "submit";
   children: ReactNode;
+  className?: string;
 }) => {
   return (
     <button
       type={type}
-      className="relative overflow-hidden rounded-lg p-2 font-medium font-inter bg-red-100 text-red-600 cursor-pointer after:content-[''] after:absolute after:left-0 after:top-0 after:w-full after:h-full after:bg-red-950 after:transition-opacity after:opacity-0 hover:after:opacity-[0.08] focus:after:opacity-[0.1] active:after:opacity-[0.16]"
+      className={`relative overflow-hidden flex items-center gap-4 rounded-lg py-3 px-6 font-medium font-inter bg-red-100 text-red-600 cursor-pointer after:content-[''] after:absolute after:left-0 after:top-0 after:w-full after:h-full after:bg-red-950 after:transition-opacity after:opacity-0 hover:after:opacity-[0.08] focus:after:opacity-[0.1] active:after:opacity-[0.16] ${className}`}
     >
       {children}
     </button>
