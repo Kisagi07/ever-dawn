5fa8460eb61f9053e7efacc5dcfca13553843d50
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-04-15 22:10:41 +0700

    feat: center content

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -13,7 +13,7 @@ const Button = ({
   return (
     <button
       type={type}
-      className={`relative overflow-hidden flex items-center gap-4 rounded-lg py-3 px-6 font-medium font-inter bg-red-100 text-red-600 cursor-pointer after:content-[''] after:absolute after:left-0 after:top-0 after:w-full after:h-full after:bg-red-950 after:transition-opacity after:opacity-0 hover:after:opacity-[0.08] focus:after:opacity-[0.1] active:after:opacity-[0.16] ${className}`}
+      className={`relative overflow-hidden flex justify-center items-center gap-4 rounded-lg py-3 px-6 font-medium font-inter bg-red-100 text-red-600 cursor-pointer after:content-[''] after:absolute after:left-0 after:top-0 after:w-full after:h-full after:bg-red-950 after:transition-opacity after:opacity-0 hover:after:opacity-[0.08] focus:after:opacity-[0.1] active:after:opacity-[0.16] ${className}`}
     >
       {children}
     </button>
