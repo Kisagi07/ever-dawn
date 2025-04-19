cbb8035e6547dda6467e87879fe5d24ed05819cf
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-04-19 14:12:15 +0700

    feat: ActionFloatingMenu (non reusable)

---
+++ app/components/ActionFloatingMenu.tsx
@@ -0,0 +1,68 @@
+import { ClockIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import { MouseEvent, ReactNode, useEffect, useRef, useState } from "react";
+import Tooltip from "./Tooltip";
+
+interface ActionFloatingMenuProps {
+  children: ReactNode;
+  addTimeOnClick: () => void;
+}
+const ActionFloatingMenu = ({
+  children,
+  addTimeOnClick,
+}: ActionFloatingMenuProps) => {
+  const [position, setPosition] = useState({ left: 0, top: 0 });
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleClicked = (e: MouseEvent<HTMLDivElement>) => {
+    const handleAutoClose = (e: globalThis.MouseEvent) => {
+      setPosition({ left: 0, top: 0 });
+      document.removeEventListener("click", handleAutoClose);
+    };
+
+    if (position.left > 0 || position.top > 0) {
+      setPosition({ left: 0, top: 0 });
+      document.removeEventListener("click", handleAutoClose);
+    } else {
+      const { clientX, clientY } = e;
+      const rect = e.currentTarget.getBoundingClientRect();
+      const x = clientX - rect.left - 20;
+      const y = clientY - rect.top - 50;
+      setPosition({ top: y, left: x });
+
+      document.addEventListener("click", handleAutoClose);
+    }
+  };
+
+  return (
+    <div
+      ref={containerRef}
+      onClick={handleClicked}
+      className={clsx("cursor-pointer relative", {
+        "after:content-[''] after:absolute  after:top-0 after:left-0 after:w-full after:h-full after:pointer-none after:bg-black after:rounded-[inherit] rounded-lg after:opacity-0 after:transition-opacity hover:after:opacity-hover focus:after:opacity-focus active:after:opacity-active":
+          position.left === 0 && position.top === 0,
+      })}
+    >
+      <div
+        className="absolute"
+        style={{
+          display: position.left > 0 || position.top > 0 ? "block" : "none",
+          left: position.left,
+          top: position.top,
+        }}
+      >
+        <Tooltip text="Add Time Spent">
+          <button
+            onClick={addTimeOnClick}
+            className="bg-white cursor-pointer rounded-full p-2 ring ring-neutral-300 shadow after:content-[''] after:absolute  after:top-0 after:left-0 after:w-full after:h-full after:bg-black after:rounded-[inherit] after:opacity-0 after:transition-opacity hover:after:opacity-hover focus:after:opacity-focus active:after:opacity-active"
+          >
+            <ClockIcon className="size-6" />
+          </button>
+        </Tooltip>
+      </div>
+      {children}
+    </div>
+  );
+};
+export default ActionFloatingMenu;
