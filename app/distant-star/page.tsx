d0682aae757246fd2804272f1284a33b53debef6
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-04-19 14:12:30 +0700

    feat: manually add spent minutes to selected star

--- app/distant-star/page.tsx
+++ app/distant-star/page.tsx
@@ -3,12 +3,16 @@ import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import BreadCrumb from "../components/BreadCrumb";
 import Button from "../components/Button";
 import FloatingText from "../components/FloatingText";
-import { FormEvent, useEffect, useRef, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { toast } from "../components/Toast";
 import redis from "../upstash";
+import ActionFloatingMenu from "../components/ActionFloatingMenu";
+import Modal from "../components/Modal";
 
 export default function Page() {
   const [stars, setStars] = useState<Star[]>([]);
+  const [openAddTimeModal, setOpenAddTimeModal] = useState(false);
+  const [addingTimeStar, setAddingTimeStar] = useState("");
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -75,53 +79,93 @@ export default function Page() {
     }
   };
 
+  const handleAddMinute = async (minute: number) => {
+    const starIndex = stars.findIndex((star) => star.name === addingTimeStar);
+    if (starIndex !== -1) {
+      try {
+        let updated = [...stars];
+        updated[starIndex].spentMinutes += minute;
+        await redis.set("stars", updated);
+        setStars(updated);
+      } catch (error) {
+        console.error(error);
+        toast(
+          "Failed in manually adding minutes spent to " + addingTimeStar,
+          "red"
+        );
+      }
+    } else {
+      toast("Adding minutes to a star that is not found");
+    }
+  };
+
+  const handleAddTimeClick = () => {
+    setOpenAddTimeModal(true);
+  };
+
   useEffect(() => {
     getStars();
   }, []);
 
   return (
-    <div className="p-2 sm:p-4 md:p-8 space-y-4">
-      <section className="max-w-7xl bg-white shadow-lg rounded-lg md:p-8 p-4 mx-auto space-y-8">
-        <div className="space-y-4">
-          <BreadCrumb />
-          <p>Each hour lights the path ahead.</p>
-        </div>
-        <form
-          onSubmit={handleSubmit}
-          className="sm:flex gap-4 space-y-4 sm:space-y-0 items-center"
-        >
-          <Button className="mt-3 w-full sm:w-60 md:w-45" type="submit">
-            <PlusCircleIcon className="size-6" />
-            New Star
-          </Button>
-          <FloatingText name="name" label="Star Name" />
-          <FloatingText
-            name="hour"
-            defaultValue="0"
-            label="Distance"
-            endOfValue="Hours"
-          />
-        </form>
-      </section>
-      <section className="max-w-7xl bg-white shadow-lg rounded-lg md:p-8 p-4 mx-auto space-y-8">
-        <div className="space-y-4">
-          <h2 className="font-bold text-xl">Your Sky</h2>
-          <p className="text-sm text-neutral-500">
-            Every star here marks a promise to yourself.
-          </p>
-        </div>
-        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {stars.map((star) => (
-            <Star
-              key={star.name}
-              name={star.name}
-              targetHours={star.targetHours}
-              spentHours={Math.floor(star.spentMinutes / 60)}
+    <>
+      <div className="p-2 sm:p-4 md:p-8 space-y-4">
+        <section className="max-w-7xl bg-white shadow-lg rounded-lg md:p-8 p-4 mx-auto space-y-8">
+          <div className="space-y-4">
+            <BreadCrumb />
+            <p>Each hour lights the path ahead.</p>
+          </div>
+          <form
+            onSubmit={handleSubmit}
+            className="sm:flex gap-4 space-y-4 sm:space-y-0 items-center"
+          >
+            <Button className="mt-3 w-full sm:w-60 md:w-45" type="submit">
+              <PlusCircleIcon className="size-6" />
+              New Star
+            </Button>
+            <FloatingText name="name" label="Star Name" />
+            <FloatingText
+              name="hour"
+              defaultValue="0"
+              label="Distance"
+              endOfValue="Hours"
             />
-          ))}
-        </div>
-      </section>
-    </div>
+          </form>
+        </section>
+        <section className="max-w-7xl bg-white shadow-lg rounded-lg md:p-8 p-4 mx-auto space-y-8">
+          <div className="space-y-4">
+            <h2 className="font-bold text-xl">Your Sky</h2>
+            <p className="text-sm text-neutral-500">
+              Every star here marks a promise to yourself.
+            </p>
+          </div>
+          <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {stars.map((star) => (
+              <ActionFloatingMenu
+                key={star.name}
+                addTimeOnClick={() => {
+                  console.log(star.name);
+                  setAddingTimeStar(star.name);
+                  handleAddTimeClick();
+                }}
+              >
+                <Star
+                  name={star.name}
+                  targetHours={star.targetHours}
+                  spentHours={Math.floor(star.spentMinutes / 60)}
+                />
+              </ActionFloatingMenu>
+            ))}
+          </div>
+        </section>
+      </div>
+      {openAddTimeModal && (
+        <Modal
+          addMinute={handleAddMinute}
+          onClose={() => setOpenAddTimeModal(false)}
+        />
+      )}
+    </>
   );
 }
 
