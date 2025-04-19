892924a5f8f9e62e27e584e4cdf808461d373dd1
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-04-19 16:16:07 +0700

    feat: make Modal component reusable

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,53 +1,38 @@
-import { FormEvent, useEffect, useRef } from "react";
+import { FormEvent, ReactNode, useEffect, useRef } from "react";
 import FloatingText from "./FloatingText";
 import Button from "./Button";
 import { toast } from "./Toast";
 
 interface ModalProps {
   onClose: () => void;
-  addMinute: (num: number) => void;
+  children?: ReactNode;
 }
 
-const Modal = ({ onClose, addMinute }: ModalProps) => {
+const Modal = ({ onClose, children }: ModalProps) => {
   const modalRef = useRef<HTMLDivElement>(null);
 
   const handleAutoClose = (e: MouseEvent) => {
     if (!modalRef.current!.contains(e.target as Node)) {
       onClose();
-      document.removeEventListener("click", handleAutoClose);
       document.body.style.overflow = "";
     }
-  };
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const minutes = Number(formData.get("minutes"));
-    if (isNaN(minutes)) {
-      toast("Minutes must be number", "red");
-    }
-    addMinute(minutes);
   };
 
   useEffect(() => {
     document.addEventListener("click", handleAutoClose);
     document.body.style.overflow = "hidden";
+
+    return () => {
+      document.removeEventListener("click", handleAutoClose);
+    };
   }, []);
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center px-2 bg-black/70 justify-center">
       <div
         ref={modalRef}
-        className="bg-white rounded-lg p-4 space-y-4 ring ring-neutral-300 w-full"
+        className="bg-white max-w-sm rounded-lg p-4 space-y-4 ring ring-neutral-300 w-full"
       >
-        <h3 className="font-medium text-xl border-b border-neutral-200">
-          Add Time
-        </h3>
-        <form className="space-y-4" onSubmit={handleSubmit}>
-          <FloatingText label="Add Minutes" name="minutes" />
-          <Button className="w-full" type="submit">
-            Add
-          </Button>
-        </form>
+        {children}
       </div>
     </div>
   );
