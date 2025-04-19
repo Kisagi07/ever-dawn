f4df39d28a6cdf5c45dbc6b60bf0327a0f5b44b2
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-04-19 17:19:45 +0700

    feat: another action for deleting

--- app/components/ActionFloatingMenu.tsx
+++ app/components/ActionFloatingMenu.tsx
@@ -1,4 +1,8 @@
-import { ArrowsRightLeftIcon, ClockIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowsRightLeftIcon,
+  ClockIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import { MouseEvent, ReactNode, useEffect, useRef, useState } from "react";
 import Tooltip from "./Tooltip";
@@ -7,11 +11,13 @@ interface ActionFloatingMenuProps {
   children: ReactNode;
   addTimeOnClick: () => void;
   onSpentMinuteCorrectionClick: () => void;
+  onDeleteClick: () => void;
 }
 const ActionFloatingMenu = ({
   children,
   addTimeOnClick,
   onSpentMinuteCorrectionClick,
+  onDeleteClick,
 }: ActionFloatingMenuProps) => {
   const [position, setPosition] = useState({ left: 0, top: 0 });
 
@@ -72,6 +78,16 @@ const ActionFloatingMenu = ({
                 className="bg-white cursor-pointer rounded-full p-2 ring ring-neutral-300 shadow after:content-[''] after:absolute  after:top-0 after:left-0 after:w-full after:h-full after:bg-black after:rounded-[inherit] after:opacity-0 after:transition-opacity hover:after:opacity-hover focus:after:opacity-focus active:after:opacity-active"
               >
                 <ArrowsRightLeftIcon className="size-6" />
+              </button>
+            </Tooltip>
+          </div>
+          <div className="absolute -translate-y-[calc(100%-60px)] translate-x-15">
+            <Tooltip text="Delete Star">
+              <button
+                onClick={onDeleteClick}
+                className="bg-rose-500 cursor-pointer text-white rounded-full p-2 ring ring-rose-300 shadow after:content-[''] after:absolute  after:top-0 after:left-0 after:w-full after:h-full after:bg-rose-950 after:rounded-[inherit] after:opacity-0 after:transition-opacity hover:after:opacity-hover focus:after:opacity-focus active:after:opacity-active"
+              >
+                <TrashIcon className="size-6" />
               </button>
             </Tooltip>
           </div>
