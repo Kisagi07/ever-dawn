0830a4101eba3a3f893d8882d7da3b89b55310b9
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-04-20 17:11:50 +0700

    feat: basic pomodoro functionality

--- app/sol-cycle/page.tsx
+++ app/sol-cycle/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BreadCrumb from "../components/BreadCrumb";
+import { toast } from "../components/Toast";
+import { send } from "process";
 
 const Page = () => {
   const [timeLeft, setTimeLeft] = useState(25 * 60);
@@ -10,8 +12,37 @@ const Page = () => {
   const formatTime = (seconds: number) => {
     const minutes = Math.floor(seconds / 60);
     const secs = seconds % 60;
-    return;
+    return `${String(minutes).padStart(2, "0")}:${String(secs).padStart(
+      2,
+      "0"
+    )}`;
   };
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    let endTime: number;
+
+    if (isRunning) {
+      endTime = Date.now() + timeLeft * 1000;
+
+      timer = setInterval(() => {
+        const newTimeLeft = Math.round((endTime - Date.now()) / 1000);
+
+        if (newTimeLeft <= 0) {
+          setTimeLeft(0);
+          setIsRunning(false);
+          // toast("Time's up!", "blue");
+          sendNotification();
+
+          clearInterval(timer);
+        } else {
+          setTimeLeft(newTimeLeft);
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
 
   const handleStart = () => {
     setIsRunning(true);
@@ -21,15 +52,34 @@ const Page = () => {
     setIsRunning(false);
   };
 
+  const sendNotification = () => {
+    if (Notification.permission === "granted") {
+      new Notification("Time's up!", {
+        body: "Your timer has finished.",
+      });
+    } else {
+      toast("Please allow notification in your browser settings.", "blue");
+    }
+  };
+
   const handleReset = () => {
     setIsRunning(false);
+    setTimeLeft(25 * 60);
   };
+
+  useEffect(() => {
+    if (Notification.permission === "default") {
+      Notification.requestPermission();
+    }
+  }, []);
 
   return (
     <div className="bg-white w-full h-screen flex items-center justify-center flex-col gap-4">
       <BreadCrumb />
       <p className=" text-primary font-medium">Drawing</p>
-      <h2 className="text-7xl font-bold text-primary">25:00</h2>
+      <h2 className="text-7xl font-bold text-primary">
+        {formatTime(timeLeft)}
+      </h2>
       {!isRunning ? (
         <button
           onClick={handleStart}
