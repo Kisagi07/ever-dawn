6df03f5ff603d24fdd4cc8d44fa58ae2a524d44d
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-05-11 13:25:43 +0700

    [Fix] Refactor layout of StarSelection component for improved structure
    Resolves [Patch] Allow Unselect Star in Sol Cycle #40

--- components/pages/sol-cycle/StarSelection.tsx
+++ components/pages/sol-cycle/StarSelection.tsx
@@ -1,7 +1,9 @@
 import StarSelect from "@/app/components/StarSelect";
 import Tooltip from "@/app/components/Tooltip";
+import { Button } from "@/components/ui/button";
 import clsx from "clsx";
 import { Dispatch, SetStateAction, useState } from "react";
+import { X } from "lucide-react";
 
 interface StarSelectionProps {
   starSelected: Star | null;
@@ -13,11 +15,16 @@ const StarSelection = ({ starSelected, setStarSelected }: StarSelectionProps) =>
 
   return (
     <div className="relative" data-testid="star-selection">
-      <Tooltip text="Change Active Star">
-        <button onClick={() => setOpenStarSelect(!openStarSelect)} className={clsx("font-medium transition-colors cursor-pointer")}>
-          {starSelected ? starSelected.name : "Select Star"}
-        </button>
-      </Tooltip>
+      <div className="flex items-center gap-2 ml-8">
+        <Tooltip text="Change Active Star">
+          <button onClick={() => setOpenStarSelect(!openStarSelect)} className={clsx("font-medium transition-colors cursor-pointer")}>
+            {starSelected ? starSelected.name : "Select Star"}
+          </button>
+        </Tooltip>
+        <Button className="size-6" size="icon" variant="outline" onClick={() => setStarSelected(null)}>
+          <X />
+        </Button>
+      </div>
       {openStarSelect && (
         <StarSelect
           starSelected={(star) => {
